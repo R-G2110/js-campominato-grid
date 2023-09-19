196972830e72e197a6e6e8e985a41faf4c5d7bd0
R-G2110 <argeldelacruz86@gmail.com>
2023-09-19 16:19:40 +0200

    [Add]: bombs on grid

--- js/script.js
+++ js/script.js
@@ -1,26 +1,59 @@
 const container = document.querySelector('.container');
 const play = document.getElementById('btn-play');
 let n;
+const bombArray = [];
 
 
 play.addEventListener('click', function(){
 
 	reset();
+	start();
+	const square = document.querySelector('.square');
+	while (bombArray.length < 16) {
+		bomb = randomIndex(1, n);
+		if (!bombArray.includes(bomb)){
+			bombArray.push(bomb);
+		} 
+	}
+	console.log(bombArray);
+
+
+
+	// for (let i = 1; i <= n*n; i++){
+	// 	if (bombArray.includes(i)){
+	// 		square.getElementById.('_squareID');
+	// 		console.log('square: '+square);
+	// 	}
+	// }
+});
+
+
+
+
+
+/********************FUNCTIONS********************************/
+// function verifyBombArray (num, array){
+// 	for(let i = 0; i < array.length; i++)
+// 	if (num === array[i]) ? num = randomIndex() : array.push;
+	
+// };
+
+
+function insertBombArray (){
+	
+};
+
+
+
+function randomIndex(min, max){
+	result = Math.floor(Math.random() * ((max*max) - min + 1) + min);
+	return result;
+};
+
+function start (){
 	inputDifficulty = document.getElementById('input-difficulty');
 	const difficulty = inputDifficulty.value;
-	switch (difficulty){
-		case "hard":
-		n = 10;
-		break;
-		case "normal":
-		n = 9;
-		break;
-		case "easy":
-		n = 7;
-		break;
-		default:
-		n = 0;
-	}
+	n = difficultyLevel(difficulty);
 
 	console.log('n: '+n);
 	console.log('difficulty: '+difficulty);
@@ -32,21 +65,46 @@ play.addEventListener('click', function(){
 	
 		square.addEventListener('click', function(){
 			this.classList.toggle('clicked');
-			console.log(this.innerHTML);
+			console.log('_squareID: '+this._squareID);
+			if (bombArray.includes(this._squareID)){
+						this.classList.add('bomb');
+						console.log('square: '+this);
+					}
 		})
 		container.append(square);
+
+		
+		
 	}
-});
+	// const bombArray = [];
+	// while (bombArray.length < 16) {
+	// 	bomb = randomIndex(1, n);
+	// 	if (!bombArray.includes(bomb)){
+	// 		bombArray.push(bomb);
+	// 		this.classList.add('bomb');
+	// 	} 
+	// }
 
+	// console.log(bombArray);
 
+}
 
-
-
-/********************FUNCTIONS********************************/
-
-function oddEven (id){
-  if(id % 2) return 'odd';
-  return 'even';
+function difficultyLevel(level){
+	let result;
+	switch (level){
+		case "easy":
+		result = 10;
+		break;
+		case "normal":
+		result = 9;
+		break;
+		case "hard":
+			result = 7;
+		break;
+		default:
+			result = 0;
+	}
+	return result;
 }
 /**
  * 
@@ -56,7 +114,7 @@ function squareCreation (index){
   const newSquare = document.createElement('div');
   newSquare.className = 'square';
   newSquare._squareID = index;
-  newSquare.innerHTML = `<span>${index}</span>`;
+  // newSquare.innerHTML = index;
 
   return newSquare;
 }
